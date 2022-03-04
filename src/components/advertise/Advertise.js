9df85080ef024457498eace349dd375be0ca2d45
nurjanahevs <evasitinurjanah2019@gmail.com>
2022-03-04 15:49:12 +0700

    ..

--- src/components/advertise/Advertise.js
+++ src/components/advertise/Advertise.js
@@ -59,23 +59,3 @@ const Advertise = () => {
 }
 
 export default Advertise;
-// const Advertise = () => {
-//   return (
-//     <div className="container">
-//       <Content/>
-//       <div className="row">
-//         <div className="column">
-          
-//         <div className="card" width="500">
-//         <img src="https://images.unsplash.com/photo-1629646526325-d4e092b641b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
-//         </div>
-//         </div>
-//         <div className="column ">
-//           <h1>Top NFT of The Week</h1>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Advertise;
