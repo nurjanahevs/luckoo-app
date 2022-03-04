57ed316fab5487811801e0ee21eebb293644f34b
nurjanahevs <evasitinurjanah2019@gmail.com>
2022-03-04 15:23:21 +0700

    ..

--- src/components/advertise/Advertise.js
+++ src/components/advertise/Advertise.js
@@ -3,18 +3,52 @@ import React from 'react'
 // import Card from './Cards/CardUI'
 import Content from '../content/Content'
 
-const Advertise = props => {
+const Advertise = () => {
   return (
     <div className="container">
       <Content/>
-    <div className="card text-center">
-      <div className="overflow">
-        <img src="https://images.unsplash.com/photo-1629646526325-d4e092b641b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt=""/>
+    <div className="row">
+      <div className="column">
+            <img src="https://images.unsplash.com/photo-1629646526325-d4e092b641b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt=""/>
       </div>
-      <div className="card-body text-dark">
+      
+      <div className="column">
+      <h2>Top NFT <br/> ------- of The Week</h2>
+      <h1>Mario Bros #001</h1>
+      <div className="topnft">
+        <p>Lorem Ipsum Collection</p>
+        <p>SotoAyam_Uenak</p>
+      </div>
 
+      <div className="current">
+        <p>Current Bid</p>
+        <h1>3.00 ETH <p>$7,282.98</p></h1>
+      </div>
+
+      <div className="auction">
+        <div className="row">
+          <div className="column">
+          <h1>02<p>Day</p></h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+          <h1>15<p>Hour</p></h1>
+          </div>
+        </div>
+        
+        <h1>15<p>Hour</p></h1>
+        <h1>20<p>Minute</p></h1>
+        <h1>18<p>Second</p></h1>
+      </div>
+
+      <p>Place a Bid <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+</svg></button></p>
+       
       </div>
     </div>
+   
     </div>
 
   )
